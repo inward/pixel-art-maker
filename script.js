1b86c8398f5022a1fe434b807151887c330bc776
inward <inw@ukr.net>
2021-03-11 13:35:55 +0200

    add sample color field

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
 let current_color = "red";
 
 canv = document.querySelector(".pix_wrapper");
-for (let i = 0; i < 50; i++){
-    for (let j = 0; j < 30; j++){
-        pix = document.createElement("div");
-        pix.className = "pixElement";
-        pix.style.left = (i * 15) + "px";
-        pix.style.top = (j * 15) + "px";
-        canv.appendChild(pix);
+brush = document.querySelector(".brush_wrapper");
+const create_pixfield = () => {
+    for (let i = 0; i < 50; i++) {
+        for (let j = 0; j < 30; j++) {
+            pix = document.createElement("div");
+            pix.className = "pixElement";
+            pix.style.left = (i * 15) + "px";
+            pix.style.top = (j * 15) + "px";
+            canv.appendChild(pix);
+        }
     }
 }
 
-canv.addEventListener('click', function(event){
+const create_paints = () =>{
+    let colors_ar = ['blue', 'green', 'red', 'purple', 'orange', 'black', 'white']
+    for (let i = 0; i < colors_ar.length; i++){
+        colorBrush = document.createElement("div");
+        colorBrush.className = "color_brush";
+        colorBrush.id = colors_ar[i];
+        colorBrush.style.backgroundColor = colors_ar[i];
+        brush.appendChild(colorBrush);
+    }
+    color_sample = document.createElement("div");
+    color_sample.id = "color_sample";
+    color_sample.style.backgroundColor = current_color;
+    brush.appendChild(color_sample);
+}
+
+canv.addEventListener('click', function (event) {
     pix = event.target;
     pix.style.backgroundColor = current_color;
 })
 
-brush = document.querySelector(".brush_wrapper");
-brush.addEventListener('click', function(event){
-    current_color = "green";
+brush.addEventListener('click', function (event) {
     current_color = event.srcElement.id;
-
+    color_sample = document.querySelector("#color_sample");
+    color_attribute = "background-color: " + current_color;
+    color_sample.setAttribute("style", color_attribute);
 })
+
+
+create_pixfield();
+create_paints();
